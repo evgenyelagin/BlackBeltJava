aace56c4aca21b0d18c786c27cc8af54421f74d8
evgenyelagin <anykeysky@gmail.com>
2021-03-11 16:51:26 +0300

    Бинарный поиск

---
+++ src/collection/BinarySearch.java
@@ -0,0 +1,26 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(-3);
+        arrayList.add(8);
+        arrayList.add(12);
+        arrayList.add(-8);
+        arrayList.add(0);
+        arrayList.add(5);
+        arrayList.add(8);
+        arrayList.add(10);
+        arrayList.add(12);
+        arrayList.add(19);
+        arrayList.add(150);
+        // сначала надо отсортировать коллекцию, иначе будет выдавать некорректный результат
+        Collections.sort(arrayList);
+        int index1 = Collections.binarySearch(arrayList, 0); /* коллекция.метод бинари сёрч(вставляем свою
+        коллекцию arrayList и элемент, который надо найти */
+        System.out.println(index1); // вывод в консоль ПОЗИЦИИ - по сути индексе, где находится int 12.
+    }
+}
